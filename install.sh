d554ae3a5426ceb0fe86b366c9150c0bf1d4159f
Moritz Gruber <m.gruber@cyclus.digital>
2020-12-31 09:22:30 +0000

    add helm

--- install.sh
+++ install.sh
@@ -18,6 +18,7 @@ brew install --cask tunnelblick
 brew install --cask rectangle
 brew install --cask 1password
 brew install k6
+brew install helm
 
 mkdir -p ~/tmp
 cd ~/Library/Fonts && curl -fLo "Droid Sans Mono for Powerline Nerd Font Complete.otf" https://github.com/ryanoasis/nerd-fonts/raw/master/patched-fonts/DroidSansMono/complete/Droid%20Sans%20Mono%20Nerd%20Font%20Complete.otf
